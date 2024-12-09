3fc6db70f56b994e25d1619335b8246c946c3bf9
semkoahmadi <ahmadahmadi9933489098@gmail.ocm>
2024-12-09 22:54:23 +0330

    debag

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -30,9 +30,9 @@ class AuthService {
     async checkOTP(mobile, code) {
         const user = await this.checkExistByMobile(mobile);
         const now = new Date().getTime();
-        if(!user?.otp?.expiresIn < now) throw new createHttpError.Unauthorized(AuthMessage.OtpCodeExpired)
-        if(!user?.otp?.code !== code) throw new createHttpError.Unauthorized(AuthMessage.OtpCodeIsIncorrect)
-            if(!user.verifiedMobile){
+        if(user?.otp?.expiresIn < now) throw new createHttpError.Unauthorized(AuthMessage.OtpCodeExpired);
+        if(user?.otp?.code !== code) throw new createHttpError.Unauthorized(AuthMessage.OtpCodeIsIncorrect);
+            if(!user.verifiedMobile) {
                 user.verifiedMobile = true;
                 await user.save();
             }
