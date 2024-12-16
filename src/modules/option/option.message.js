fd21f9108e54a1c7ff09141c5511b598b9d4c1ac
Semkoahmadi <ahmadahmadi9933489098@gmail.com>
2024-12-16 16:10:31 +0330

    last change for option

--- src/modules/option/option.message.js
+++ src/modules/option/option.message.js
@@ -1,9 +1,11 @@
 const OptionMessage = Object.freeze({
     Created: "option created successfully",
     NotFound: "option not found",
-    AlreadyExist: "option already exist"
-
+    AlreadyExist: "option already exist",
+    Deleted: "option deleted successfully",
+    Updated: "option updated successfully",
 });
+
 module.exports = {
     OptionMessage
 }
--- src/modules/post/option.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const OptionController = require("./option.controller");
-const router = Router();
-router.post("/", OptionController.create)
-router.get("/by-category/:categoryId", OptionController.findByCategoryId)
-router.get("/:id", OptionController.findById)
-router.get("/", OptionController.find)
-module.exports = { OptionRouter: router };
